3d45423cd25a608283d60dfb40f9e13046a45f38
TFCMarques <tfcmarques98@gmail.com>
2020-12-02 00:15:03 +0000

    Finished staff login and website footer

---
+++ commons/footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Footer</title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com/ ">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        
+        <link rel="icon" href="../img/icon.ico" type="image/icon type">
+    
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    </head>
+
+    <body>
+        <div class="footer">
+            <img src="../img/logo-white.png" alt="Logo" height="15px">
+            <p>Project developed for Medical Information Systems Course @ FCT-NOVA</p>
+            <p>Copyright &copy; 2020</p>
+        </div>
+    </body>
+</html>
--- login/staffLogin.php
+++ login/staffLogin.php
@@ -1,41 +1,55 @@
 <!DOCTYPE html>
 
-<head>
-    <title>COVIDSYM - Staff Login</title>
+<html>
+    <head>
+        <title>COVIDSYM - Staff Login</title>
 
-    <link rel="preconnect" href="https://fonts.gstatic.com/ ">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com/ ">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        
+        <link rel="icon" href="../img/icon.ico" type="image/icon type">
     
-    <link rel="icon" href="../img/icon.ico" type="image/icon type">
+        <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    </head>
 
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="../css/login.css">
-    <link rel="stylesheet" type="text/css" href="../css/footer.css">
-</head>
-
-<body>
-    <div class="wrapper">
-        <div class="title-container">
+    <body>
+        <div class="image-wrapper">
             <img src="../img/logo.png" alt="Logo" height="50px" style="margin: auto;">
         </div>
-    
-        <div class="login-container">
-            <div class="header-wrapper">
-                <div class="header">
-                    <h4>Staff Login</h4>
-                </div>
+
+        <div class="login-wrapper">
+            <div class="header">
+                Staff Login
             </div>
-    
-            <div class="login-wrapper">
-                <div class="login">
-                    <form class="form" action="/login/checkLogin.php" method="POST">
-                        <input class="text-input" type="text" name="username" placeholder="Username">
-                        <input class="text-input" type="password" name="password" placeholder="Password">
-                        <a href="#">Recuperar Password</a>
-                        <input class="button" type="submit" value="Login">
-                    </form>
+
+            <form action="../login/checkLogin.php" method="POST">
+                <div class="field">
+                    <input type="text" name="Email" required>
+                    <label>Email</label>
                 </div>
-            </div>
+
+                <div class="field">
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+
+                <div class="content">
+                    <div class="checkbox">
+                        <input type="checkbox" id="remember-me">
+                        <label for="remember-me">Remember me</label>
+                    </div>
+
+                    <div class="pass-link">
+                        <a href="#">Forgot your password?</a>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <input type="submit" value="Login">
+                </div>
+            </form>
         </div>
-    </div>
-</body>
+    </body>
+</html>
