3d417c0f58b58629640fdb13df896b092e49ea1a
TFCMarques <tfcmarques98@gmail.com>
2020-12-06 21:27:41 +0000

    Sidebar

---
+++ commons/sidebar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Sidebar</title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com/ ">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        
+        <link rel="icon" href="../img/icon.ico" type="image/icon type">
+    
+        <link rel="stylesheet" type="text/css" href="../css/sidebar.css">
+    </head>
+
+    <body>
+        <div class="sidebar">
+            <a class="sidebar-button" href="#">
+                <i class="fas fa-home icon"></i>
+                Main Menu
+            </a>
+            <a class="sidebar-button" href="#">
+                <i class="far fa-id-card icon"></i>    
+                Profile
+            </a>
+            <a class="sidebar-button" href="#">
+                <i class="far fa-calendar-check icon"></i>    
+                Appointments
+            </a>
+            <a class="sidebar-button" href="#">
+                <i class="fas fa-stethoscope icon"></i>     
+                Diagnosis
+            </a>
+            <a class="sidebar-button" href="#">
+                <i class="far fa-question-circle icon"></i> 
+                FAQ
+            </a>
+        </div>
+    </body>
+</html>
