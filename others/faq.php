c5324a6316dff98e9ec34afdba537fa1039faac3
jrmduarte23 <jr.duarte@campus.fct.unl.pt>
2020-12-08 02:36:02 +0000

    Added FAQ and User Profile

---
+++ others/faq.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+    <title>COVIDSYM - FAQ</title>
+
+    <link rel="icon" href="../img/icon.ico" type="image/icon type">
+
+    <link rel="stylesheet" href="../css/faq.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+<body>
+    <?php
+        include "../commons/navbar.php";
+    ?>
+
+    <div class="wrapper">
+        <div>
+            <?php include "../commons/sidebar.php"; ?>
+        </div>
+
+        <div class="modal">
+            <div class="modal-header">
+                <i class="fas fa-arrow-alt-circle-left goBackBtn">
+                    <a href="#"></a>
+                </i>
+                <h1 class="title">FAQ</h1>
+            </div>
+            <div >
+                <table>
+                    <tr>
+                        <th class="info">
+                            <h1>Frequently Asked Questions</h1>
+                            <div class="info-text">
+                                <p>The Frequently Asked Questions page aims to address the issues considered most
+                                    important by our clients.</p><br>
+                                <p>To find answers to your questions choose one of the question categories and identify
+                                    your question.</p><br>
+                                <p>If your question is not answered on this page, do not hesitate to contact us.</p><br>
+
+                            </div>
+                        </th>
+                        <th class="faq">
+                            <div class="acc-kontainer">
+                                <div>
+                                    <input type="radio" name="acc" id="acc1">
+                                    <label for="acc1"> My name? <i class="fas fa-chevron-down down"></i></label>
+                                    <div class="acc-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet semper velit. Duis vel ex tincidunt, gravida libero sed, fermentum enim. Vivamus non risus sed urna fermentum aliquet. Ut tincidunt diam et orci auctor facilisis. In hac habitasse platea dictumst. Praesent id varius lectus. Duis facilisis viverra turpis ac consequat.
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="radio" name="acc" id="acc2">
+                                    <label for="acc2"> What am I interesting for?<i class="fas fa-chevron-down down"></i></label>
+                                    <div class="acc-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet semper velit. Duis vel ex tincidunt, gravida libero sed, fermentum enim. Vivamus non risus sed urna fermentum aliquet. Ut tincidunt diam et orci auctor facilisis. In hac habitasse platea dictumst. Praesent id varius lectus. Duis facilisis viverra turpis ac consequat.
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="radio" name="acc" id="acc3">
+                                    <label for="acc3"> What is my hobby?<i class="fas fa-chevron-down down"></i></label>
+                                    <div class="acc-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet semper velit. Duis vel ex tincidunt, gravida libero sed, fermentum enim. Vivamus non risus sed urna fermentum aliquet. Ut tincidunt diam et orci auctor facilisis. In hac habitasse platea dictumst. Praesent id varius lectus. Duis facilisis viverra turpis ac consequat.
+                                    </div>
+                                </div>
+                            </div>
+                        </th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <?php
+      include('../commons/footer.php');
+    ?>
+</body>
+
+</html>
