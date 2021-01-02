eff6e3de065203fe03167f662747ab1ae9531b72
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2021-01-02 18:40:23 +0000

    Small User Signup fixes and User Login responsiveness

--- login/userLogin.php
+++ login/userLogin.php
@@ -14,61 +14,61 @@
 
     <body>
         <?php include "../commons/navbar.php"; ?>
-        
-        <div class="image-wrapper">
-            <img src="../img/logo.png" alt="Logo">
-        </div>
 
         <div class="container">
-            <div class="login-wrapper">
-                <div class="header">
-                    User Login
-                </div>
-
-                <form action="../login/checkLogin.php" method="POST">
-                    <div class="field">
-                        <input type="text" name="Email" required>
-                        <label>Email</label>
-                    </div>
-
-                    <div class="field">
-                        <input type="password" required>
-                        <label>Password</label>
-                    </div>
-
-                    <div class="content">
-                        <div class="checkbox">
-                            <input type="checkbox" id="remember-me">
-                            <label for="remember-me">Remember me</label>
-                        </div>
-
-                        <div class="pass-link">
-                            <a href="#">Forgot your password?</a>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <input type="submit" value="Login">
-                    </div>
-
-                    <div class="signup-link">
-                            <a href="#">Don't have an account yet?</a>
-                        </div>
-                </form>
+            <div class="image-wrapper">
+                <img src="../img/logo.png" alt="Logo">
             </div>
 
-            <div class="info-wrapper">
-                <h1>What is COVIDSYM?</h1>
-                <p class="info-text">
-                    COVIDSYM consists in a symptom and risk factor registry and analysis system, that supports medical staff in deciding to prescribe a test to COVID-19, based on the assessment of the user's risk of being diagnosed with it. The risk assessment is done automatically based on the indicators registered by the user, using a high-fidelity Diagnostic Support System.
-                </p>
-                <p class="more-info">For more information:</p>
-                <a href="#">Frequently asked questions</a>
+            <div class="main">
+                <div class="login-wrapper">
+                    <div class="header">
+                        User Login
+                    </div>
+
+                    <form action="../login/checkLogin.php" method="POST">
+                        <div class="field">
+                            <input type="text" name="Email" required>
+                            <label>Email</label>
+                        </div>
+
+                        <div class="field">
+                            <input type="password" required>
+                            <label>Password</label>
+                        </div>
+
+                        <div class="content">
+                            <div class="checkbox">
+                                <input type="checkbox" id="remember-me">
+                                <label for="remember-me">Remember me</label>
+                            </div>
+
+                            <div class="pass-link">
+                                <a href="#">Forgot your password?</a>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <input type="submit" value="Login">
+                        </div>
+
+                        <div class="signup-link">
+                            <a href="#">Don't have an account yet?</a>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="info-wrapper">
+                    <h1>What is COVIDSYM?</h1>
+                    <p class="info-text">
+                        COVIDSYM consists in a symptom and risk factor registry and analysis system, that supports medical staff in deciding to prescribe a test to COVID-19, based on the assessment of the user's risk of being diagnosed with it. The risk assessment is done automatically based on the indicators registered by the user, using a high-fidelity Diagnostic Support System.
+                    </p>
+                    <p class="more-info">For more information:</p>
+                    <a href="#">Frequently asked questions</a>
+                </div>
             </div>
         </div>
 
-        <?php
-            include "../commons/footer.php";
-        ?>
+        <?php include "../commons/footer.php";?>
     </body>
 </html>
