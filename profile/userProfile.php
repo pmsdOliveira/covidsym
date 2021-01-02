fffdb3dbc765271752324adbbb359eec6aebf2b3
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2021-01-02 19:41:30 +0000

    User Profile responsiveness

--- profile/userProfile.php
+++ profile/userProfile.php
@@ -28,135 +28,133 @@
       <div class="content-wrapper">
         <div class="modal">
           <div class="modal-header">
+            <i class="fas fa-arrow-alt-circle-left goBackBtn">
+              <a href="#"></a>
+            </i>
             <h1>User Profile</h1>
           </div>
 
           <table class="table">
             <tr>
-              <th class="table_left">
+              <td class="table_left">
                 <div class="profile">
                   <h2>[Username]</h2>
                   <i class="fas fa-user-circle profileIcon"></i>
                   <button>Update Picture</button>
-                  <button>Edit Profile</button>
-                  <button>Change Password</button>
-                  <button>Delete Account</button>
                 </div>
-              </th>
-              <th>
-                <div>
-                  <form action="submitUserRegistry.php" method="POST">
-                    <table class="form">
-                      <tr>
-                        <td><label>Name</label></td>
-                        <td style="width: 70%">
-                          <input name="name" value="Name from DB" type="text" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Email</label></td>
-                        <td>
-                          <input
-                            name="email"
-                            value="Email from DB"
-                            type="email"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Phone Number</label></td>
-                        <td>
-                          <input
-                            name="phone"
-                            value="XXXXXXXXXX from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Address</label></td>
-                        <td>
-                          <input
-                            name="address"
-                            value="Adress from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Local</label></td>
-                        <td>
-                          <input
-                            name="local"
-                            value="Local from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>District</label></td>
-                        <td>
-                          <input
-                            name="district"
-                            value="District from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Birthdate</label></td>
-                        <td><input name="birthdate" type="date" /></td>
-                      </tr>
-                      <tr>
-                        <td><label>Gender</label></td>
-                        <td>
-                          <div class="gender">
-                            <label
-                              >Male<input
-                                name="gender"
-                                type="radio"
-                                value="male"
-                            /></label>
-                            <label
-                              >Female<input
-                                name="gender"
-                                type="radio"
-                                value="female"
-                            /></label>
-                            <label
-                              >Other<input
-                                name="gender"
-                                type="radio"
-                                value="other"
-                            /></label>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Fiscal Number</label></td>
-                        <td>
-                          <input
-                            name="fiscal"
-                            value="XXXXXXXX from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><label>Healthcare Number</label></td>
-                        <td>
-                          <input
-                            name="healthcare"
-                            value="XXXXXXXX from DB"
-                            type="text"
-                          />
-                        </td>
-                      </tr>
-                    </table>
-                    <input type="submit" value="Submit" />
-                  </form>
-                </div>
-              </th>
+              </td>
+              <td>
+                <form action="submitUserRegistry.php" method="POST">
+                  <table class="form">
+                    <tr>
+                      <td><label>Name</label></td>
+                      <td>
+                        <input name="name" value="Name from DB" type="text" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Email</label></td>
+                      <td>
+                        <input
+                          name="email"
+                          value="Email from DB"
+                          type="email"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Phone Number</label></td>
+                      <td>
+                        <input
+                          name="phone"
+                          value="XXXXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Address</label></td>
+                      <td>
+                        <input
+                          name="address"
+                          value="Adress from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Local</label></td>
+                      <td>
+                        <input
+                          name="local"
+                          value="Local from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>District</label></td>
+                      <td>
+                        <input
+                          name="district"
+                          value="District from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Birthdate</label></td>
+                      <td><input name="birthdate" type="date" /></td>
+                    </tr>
+                    <tr>
+                      <td><label>Gender</label></td>
+                      <td>
+                        <div class="gender">
+                          <label
+                            >Male<input
+                              name="gender"
+                              type="radio"
+                              value="male"
+                          /></label>
+                          <label
+                            >Female<input
+                              name="gender"
+                              type="radio"
+                              value="female"
+                          /></label>
+                          <label
+                            >Other<input
+                              name="gender"
+                              type="radio"
+                              value="other"
+                          /></label>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Fiscal Number</label></td>
+                      <td>
+                        <input
+                          name="fiscal"
+                          value="XXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Healthcare Number</label></td>
+                      <td>
+                        <input
+                          name="healthcare"
+                          value="XXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                  </table>
+                  <input type="submit" value="Update Profile" />
+                </form>
+              </td>
             </tr>
           </table>
         </div>
