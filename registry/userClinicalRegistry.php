6bd9a79ff8ea8e92f54a634192b0f53fce582511
jrmduarte23 <jr.duarte@campus.fct.unl.pt>
2021-01-03 18:13:59 +0000

    user signup next step added

--- registry/userClinicalRegistry.php
+++ registry/userClinicalRegistry.php
@@ -1,92 +1,168 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <title>COVIDSYM - User Clinical Registry</title>
+  <head>
+    <title>COVIDSYM - User Clinical Registry</title>
 
-        <link rel="icon" href="../img/icon.ico" type="image/icon type">
+    <link rel="icon" href="../img/icon.ico" type="image/icon type" />
 
-        <link rel="stylesheet" href="../css/registry.css">
-        <link rel="preconnect" href="https://fonts.gstatic.com/">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    </head>
+    <link rel="stylesheet" href="../css/registry.css" />
 
-    <body>
-        <?php include "../commons/navbar.php"; ?>
+    <link rel="preconnect" href="https://fonts.gstatic.com/" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    />
+  </head>
 
-        <div class="wrapper">
-            <?php include "../commons/sidebar.php"; ?>
+  <body>
+    <?php include "../commons/navbar.php"; ?>
 
-            <div class="content-wrapper">
-                <div class="modal">
-                    <div class="modal-header">
-                        <i class="fas fa-arrow-alt-circle-left goBackBtn">
-                            <a href="#"></a>
-                        </i>
-                        <h1 class="title">User Clinical Registry</h1>
-                    </div>
-                    <form action="submitUserRegistry.php" method="POST">
-                        <table class="form">
-                            <tr>
-                                <td><label>Name</label></td>
-                                <td style="width: 70%;"><input name="name" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Username</label></td>
-                                <td><input name="username" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Email</label></td>
-                                <td><input name="email" type="email"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Phone Number</label></td>
-                                <td><input name="phone" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Address</label></td>
-                                <td><input name="address" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Local</label></td>
-                                <td><input name="local" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>District</label></td>
-                                <td><input name="district" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Birthdate</label></td>
-                                <td><input name="birthdate" type="date"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Gender</label></td>
-                                <td>
-                                    <div class="gender">
-                                        <label>Male<input name="gender" type="radio" value="male"></label>
-                                        <label>Female<input name="gender" type="radio" value="female"></label>
-                                        <label>Other<input name="gender" type="radio" value="other"></label>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><label>Fiscal Number</label></td>
-                                <td><input name="fiscal" type="text"></td>
-                            </tr>
-                            <tr>
-                                <td><label>Healthcare Number</label></td>
-                                <td><input name="healthcare" type="text"></td>
-                            </tr>
-                        </table>
-                        <input type="submit" value="Submit">
-                    </form>
+    <div class="wrapper">
+      <?php include "../commons/sidebar.php"; ?>
+
+      <div class="content-wrapper">
+        <div class="modal">
+          <div class="modal-header">
+            <i class="fas fa-arrow-alt-circle-left goBackBtn">
+              <a href="#"></a>
+            </i>
+            <h1>Personal Information</h1>
+          </div>
+
+          <table class="table">
+            <tr>
+              <td class="table_left">
+                <div class="profile">
+                  <h2>[Username]</h2>
+                  <i class="fas fa-user-circle profileIcon"></i>
+                  <button>Update Picture</button>
+                  <button>Cancel</button>
                 </div>
-            </div>
+              </td>
+              <td>
+                <form action="submitUserRegistry.php" method="POST">
+                  <table class="form">
+                    <tr>
+                      <td><label>Name</label></td>
+                      <td>
+                        <input
+                          name="name"
+                          placeholder="Name from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Email</label></td>
+                      <td>
+                        <input
+                          name="email"
+                          placeholder="Email from DB"
+                          type="email"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Phone Number</label></td>
+                      <td>
+                        <input
+                          name="phone"
+                          placeholder="XXXXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Address</label></td>
+                      <td>
+                        <input
+                          name="address"
+                          placeholder="Adress from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Local</label></td>
+                      <td>
+                        <input
+                          name="local"
+                          placeholder="Local from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>District</label></td>
+                      <td>
+                        <input
+                          name="district"
+                          placeholder="District from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Birthdate</label></td>
+                      <td><input name="birthdate" type="date" /></td>
+                    </tr>
+                    <tr>
+                      <td><label>Gender</label></td>
+                      <td>
+                        <div class="gender">
+                          <label
+                            >Male<input name="gender" type="radio" value="male"
+                          /></label>
+                          <label
+                            >Female<input
+                              name="gender"
+                              type="radio"
+                              value="female"
+                          /></label>
+                          <label
+                            >Other<input
+                              name="gender"
+                              type="radio"
+                              value="other"
+                          /></label>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Fiscal Number</label></td>
+                      <td>
+                        <input
+                          name="fiscal"
+                          placeholder="XXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><label>Healthcare Number</label></td>
+                      <td>
+                        <input
+                          name="healthcare"
+                          placeholder="XXXXXXXX from DB"
+                          type="text"
+                        />
+                      </td>
+                    </tr>
+                  </table>
+                  <input type="submit" value="Submit" />
+                </form>
+              </td>
+            </tr>
+          </table>
         </div>
+      </div>
+    </div>
 
-        <?php include('../commons/footer.php'); ?>
-
-        <script src="../js/userClinicalRegistry.js"></script>
-    </body>
+    <?php include('../commons/footer.php'); ?>
+  </body>
 </html>
