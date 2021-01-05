7534c4bab35b9c11552cae421761ac424c91e734
jrmduarte23 <jr.duarte@campus.fct.unl.pt>
2021-01-05 00:36:32 +0000

    some visual fixes

--- info/termsOfService.php
+++ info/termsOfService.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
   <head>
     <title>COVIDSYM - Terms Of Service</title>
@@ -15,7 +16,6 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
     />
   </head>
-
   <body>
     <?php
         include "../commons/navbar.php";
@@ -29,70 +29,68 @@
         <h1>Terms Of Service</h1>
       </div>
       <div class="modal-body">
-        <div class="scroll-box">
-          <div class="info">
-            <p>
-              This Website was created, belongs to and is operated by a group of
-              students of FTC-NOVA, it's part of a final project for the subject
-              "Sistemas de Informação Médica"
-            </p>
-            <h1>Acceptance of Terms of Service</h1>
-            <p>
-              The Website user (hereinafter referred to as the "User")
-              acknowledges that by using this Website they are accepting these
-              Terms of Service.
-            </p>
-            <h1>Amendments to the Terms of Service</h1>
-            <p>
-              The group reserves the right to change, add, update or delete
-              these Terms of Service, partially or totally, at any time without
-              prior notice and with immediate effect. The User should
-              periodically consult the page of these Terms of Service to confirm
-              if any updates or changes have been made. If they do not agree
-              with any of the rules of use, they should not use the Website.
-            </p>
-            <h1>Access to the Website</h1>
-            <p>
-              Access to the Website is not subject to registration. The group
-              has the exclusive right at all times to suspend access to the
-              Website, partially or totally, especially for management,
-              maintenance, repair, alteration or update operations and to close
-              down the Website or any of the services, permanently or
-              temporarily, partially or totally, at any time, at its own
-              discretion and without prior notice.
-            </p>
-            <h1>Personal Data</h1>
-            <p>
-              Use of this Website does not necessarily imply the provision of
-              personal data. However, if you wish to request contact and
-              clarification, submit a complaint or make comments or suggestions,
-              you must provide us with some data, including your name and
-              contact information, which will be treated as defined in the
-              Privacy Policy available on this Website.
-            </p>
-            <h1>Security</h1>
-            <p>
-              The Group does not guarantee that the Website will operate
-              uninterruptedly, be error-free or fault-free, or be available
-              continuously.
-            </p>
-            <p>
-              The Group makes every effort to ensure that the Website does not
-              contain any viruses or other similar items that are dangerous to
-              your computer or mobile device. However, since The Group cannot
-              fully control the circulation of information over the Internet, it
-              cannot guarantee that it does not contain any type of virus or
-              other items that may damage your computer or mobile device.
-            </p>
-            <p>
-              To ensure the security of the Website, The Group may, at any time
-              and without prior notice, take the necessary steps to ensure the
-              integrity, security, continuity or quality of the Website,
-              including restrictions or limitations on access.
-            </p>
-            <br />
-            <p>Last update: 29 December 2020</p>
-          </div>
+        <div class="info">
+          <p>
+            This Website was created, belongs to and is operated by a group of
+            students of FTC-NOVA, it's part of a final project for the subject
+            "Sistemas de Informação Médica"
+          </p>
+          <h1>Acceptance of Terms of Service</h1>
+          <p>
+            The Website user (hereinafter referred to as the "User")
+            acknowledges that by using this Website they are accepting these
+            Terms of Service.
+          </p>
+          <h1>Amendments to the Terms of Service</h1>
+          <p>
+            The group reserves the right to change, add, update or delete these
+            Terms of Service, partially or totally, at any time without prior
+            notice and with immediate effect. The User should periodically
+            consult the page of these Terms of Service to confirm if any updates
+            or changes have been made. If they do not agree with any of the
+            rules of use, they should not use the Website.
+          </p>
+          <h1>Access to the Website</h1>
+          <p>
+            Access to the Website is not subject to registration. The group has
+            the exclusive right at all times to suspend access to the Website,
+            partially or totally, especially for management, maintenance,
+            repair, alteration or update operations and to close down the
+            Website or any of the services, permanently or temporarily,
+            partially or totally, at any time, at its own discretion and without
+            prior notice.
+          </p>
+          <h1>Personal Data</h1>
+          <p>
+            Use of this Website does not necessarily imply the provision of
+            personal data. However, if you wish to request contact and
+            clarification, submit a complaint or make comments or suggestions,
+            you must provide us with some data, including your name and contact
+            information, which will be treated as defined in the Privacy Policy
+            available on this Website.
+          </p>
+          <h1>Security</h1>
+          <p>
+            The Group does not guarantee that the Website will operate
+            uninterruptedly, be error-free or fault-free, or be available
+            continuously.
+          </p>
+          <p>
+            The Group makes every effort to ensure that the Website does not
+            contain any viruses or other similar items that are dangerous to
+            your computer or mobile device. However, since The Group cannot
+            fully control the circulation of information over the Internet, it
+            cannot guarantee that it does not contain any type of virus or other
+            items that may damage your computer or mobile device.
+          </p>
+          <p>
+            To ensure the security of the Website, The Group may, at any time
+            and without prior notice, take the necessary steps to ensure the
+            integrity, security, continuity or quality of the Website, including
+            restrictions or limitations on access.
+          </p>
+          <br />
+          <p>Last update: 29 December 2020</p>
         </div>
       </div>
     </div>
--- profile/staffList.php
+++ profile/staffList.php
@@ -1,65 +1,73 @@
+<!DOCTYPE html>
 <html>
+  <head>
+    <title>COVIDSYM - Staff List</title>
 
-    <head>
-        <title>COVIDSYM - Staff List</title>
+    <link rel="icon" href="../img/icon.ico" type="image/icon type" />
 
-        <link rel="icon" href="../img/icon.ico" type="image/icon type">
+    <link rel="stylesheet" href="../css/staffList.css" />
 
-        <link rel="stylesheet" href="../css/staffList.css">
-        <link rel="preconnect" href="https://fonts.gstatic.com/">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    </head>
+    <link rel="preconnect" href="https://fonts.gstatic.com/" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    />
+  </head>
 
-    <body>
-        <?php include "../commons/navbar.php"; ?>
+  <body>
+    <?php include "../commons/navbar.php"; ?>
 
-        <div class="wrapper">
-            <?php include "../commons/sidebar.php"?>
+    <div class="wrapper">
+      <?php include "../commons/sidebar.php"; ?>
 
-            <div class="content-wrapper">
-                <div class="modal">
-                    <div class="modal-header">
-                        <i class="fas fa-arrow-alt-circle-left goBackBtn">
-                            <a href="#"></a>
-                        </i>
-                        <h1>Staff</h1>
-                    </div>
-                    <div class=" staff-list">
-                        <div class="topSide">
-                            <p class="p">Select a Staff Member to see his profile:</p>     
-                            <select name="staff" id="staff">
-                                <option value="Medic">Medic</option>
-                                <option value="Investigator">Investigator</option>
-                                <option value="Admin">Admin</option>
-                            </select>
-                        </div>
-                        <?php
-                            for ($i = 0; $i < 5; $i++) {
-                                echo '<div class=" staff">
-                                        <i class="fas fa-user-circle"></i>
-                                        <p>Dr. Albertino da Conceição</p>
+      <div class="content-wrapper">
+        <div class="modal">
+          <div class="modal-header">
+            <i class="fas fa-arrow-alt-circle-left goBackBtn">
+              <a href="#"></a>
+            </i>
+            <h1>Staff</h1>
+          </div>
+
+          <div class="staffs-header">
+            <p>Select a Staff Member to see his profile:</p>     
+            <select name="staff" id="staff">
+                <option value="Medic">Medic</option>
+                <option value="Investigator">Investigator</option>
+                <option value="Admin">Admin</option>
+            </select>
+          </div>
+          <div class="staffs-list">
+            <?php
+                for ($i = 0; $i < 5; $i++) {
+                    echo '<div class=" staff">
+                            <i class="fas fa-user-circle"></i>
+                            <p>Albertino da Conceição</p>
                                         
-                                        <button>Select</button>
-                                    </div>';
-                            }
-                        ?>
-                    </div>
-                    <div class="pages">
-                        <a class="currentPage">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
-                        <a href="#">4</a>
-                        <a href="#">5</a>
-                        <a href="#">></a>
-                    </div>
-                    <div class="buttonSide">
-                        <button>Create new Staff Member</button>
-                    </div>
-                </div>
+                            <button class="button">Select</button>
+                        </div>';
+                }
+                ?>
+          </div>
+          <div class="pages">
+                <a class="currentPage">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">></a>
             </div>
+          <div class="staff-button">
+            <a href="#">Create New Staff Member</a>
+          </div>
         </div>
-        <?php include('../commons/footer.php'); ?>
-    </body>
+      </div>
+    </div>
 
+    <?php include('../commons/footer.php'); ?>
+  </body>
 </html>
