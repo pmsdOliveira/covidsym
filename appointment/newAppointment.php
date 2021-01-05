aa1be67f9d5e6c9660fdef369d29f2a48b714cb5
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2021-01-05 22:04:28 +0000

    New appointment backend

--- appointment/newAppointment.php
+++ appointment/newAppointment.php
@@ -13,6 +13,23 @@
     </head>
 
     <body>
+        <?php
+            include "../commons/config.php";
+            if (isset($_GET["page"]))
+                $pageNumber = $_GET["page"];
+            else
+                $pageNumber = 1;
+            $firstResult = ($pageNumber - 1) * 5;
+
+            $query = "SELECT * FROM medic";
+            $result = mysqli_query($connect, $query)
+                or die(mysqli_error($connect));
+            $nPages = intval(mysqli_num_rows($result) / 5 + 1);
+            
+            $query = "SELECT medic.id, medic.name FROM medic WHERE medic.id > " . $firstResult;
+            $result = mysqli_query($connect, $query)
+                or die(mysqli_error($connect));
+        ?>
         <?php include "../commons/navbar.php"; ?>
 
         <div class="wrapper">
@@ -26,26 +43,37 @@
                         </i>
                         <h1 class="title">New Appointment</h1>
                     </div>
+                    
                     <div class="medic-list">
                         <p>Select which doctor should do the consultation:</p>
                         <?php
                             for ($i = 0; $i < 5; $i++) {
-                                echo '<div class="medic">
-                                        <i class="fas fa-user-circle"></i>
-                                        <p>Hipólito Asdrubal</p>
-                                        <p>Users waiting: XX</p>
-                                        <button>Select</button>
-                                    </div>';
+                                echo '<div class="medic">';
+                                if ($medic = mysqli_fetch_array($result)) {
+                                    $countQuery = 'SELECT count(appointment.medic_id) AS waiting_list 
+                                        FROM medic INNER JOIN appointment ON medic.id = appointment.medic_id 
+                                        WHERE medic.id =' . (($pageNumber - 1) * 5 + ($i + 1));
+                                    $countResult = mysqli_query($connect, $countQuery)
+                                        or die(mysqli_error($connect));
+                                    $waitingList = mysqli_fetch_array($countResult);
+                                    echo '<p>' . $medic["name"] . '</p>
+                                        <p>Users waiting: ' .$waitingList["waiting_list"] . '</p>
+                                        <button>Select</button>';
+                                }
+                            echo '</div>';
                             }
                         ?>
                     </div>
+                    
                     <div class="pages">
-                        <a class="currentPage">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
-                        <a href="#">4</a>
-                        <a href="#">5</a>
-                        <a href="#">></a>
+                    <?php
+                        for ($i = 1; $i <= $nPages; $i++) {
+                            if ($i == $pageNumber)
+                                echo '<p>' . $i . '</p>';
+                            else
+                                echo '<a href="newAppointment?page=' . $i . '">' . $i . '</a>';
+                        }
+                    ?>
                     </div>
                 </div>
             </div>
